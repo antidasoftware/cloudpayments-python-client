9e6c8ed756e9970b6881e3783a0433313383ef93
Igor Shevchenko <mail@igorshevchenko.ru>
2016-05-26 16:04:23 +0500

    Добавил методы для создания, изменения и отмены подписок

--- cloudpayments.py
+++ cloudpayments.py
@@ -92,14 +92,14 @@ class CloudPayments(object):
 
         raise CloudPaymentsError(response)
 
-    def charge_token(self, account_id, token, amount, currency,
+    def charge_token(self, token, account_id, amount, currency,
                      ip_address=None, invoice_id=None, description=None,
                      email=None, data=None):
         params = {
             'Amount': amount,
             'Currency': currency,
             'AccountId': account_id,
-            'Token': token,
+            'Token': token
         }
         if invoice_id is not None:
             params['InvoiceId'] = invoice_id
@@ -119,3 +119,66 @@ class CloudPayments(object):
         if 'Model' in response and 'ReasonCode' in response['Model']:
             raise PaymentError(response)
         raise CloudPaymentsError(response)
+
+    def create_subscription(self, token, account_id, amount, currency,
+                            description, email, start_date, interval, period,
+                            require_confirmation=False, max_periods=None):
+        params = {
+            'Token': token,
+            'AccountId': account_id,
+            'Description': description,
+            'Email': email,
+            'Amount': amount,
+            'Currency': currency,
+            'RequireConfiramtion': require_confirmation,
+            'StartDate': start_date.isoformat(),
+            'Interval': interval,
+            'Period': period,
+        }
+        if max_periods is not None:
+            params['MaxPeriods'] = max_periods
+
+        response = self._send_request('subscriptions/create', params)
+
+        if response['Success']:
+            return response['Model']
+        raise CloudPaymentsError(response)
+
+    def update_subscription(self, subscription_id, amount=None, currency=None,
+                            description=None, start_date=None, interval=None,
+                            period=None, require_confirmation=None,
+                            max_periods=None):
+        params  = {
+            'Id': subscription_id
+        }
+        if description is not None:
+            params['Description'] = description
+        if amount is not None:
+            params['Amount'] = amount
+        if currency is not None:
+            params['Currency'] = currency
+        if require_confirmation is not None:
+            params['RequireConfirmation'] = require_confirmation
+        if start_date is not None:
+            params['StartDate'] = start_date.isoformat()
+        if interval is not None:
+            params['Interval'] = interval
+        if period is not None:
+            params['Period'] = period
+        if max_periods is not None:
+            params['MaxPeriods'] = max_periods
+
+        response = self._send_request('subscriptions/update', params)
+
+        if response['Success']:
+            return response['Model']
+        raise CloudPaymentsError(response)
+
+    def cancel_subscription(self, subscription_id):
+        params = {'SubscriptionId': subscription_id}
+
+        response = self._send_request('subscriptions/cancel', params)
+
+        if not response['Success']:
+            raise CloudPaymentsError(response)
+
