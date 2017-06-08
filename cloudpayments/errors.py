7bcdd4a5474c6370200bf0b49a3cd745acc73d9e
Igor Shevchenko <mail@igorshevchenko.ru>
2017-06-08 19:18:28 +0500

    #1 Добавил человекочитаемые сообщения об ошибках

--- cloudpayments/errors.py
+++ cloudpayments/errors.py
@@ -1,3 +1,6 @@
+from .enums import ERROR_MESSAGES
+
+
 class CloudPaymentsError(Exception):
     def __init__(self, response, message=None):
         self.response = response
@@ -11,3 +14,15 @@ class PaymentError(CloudPaymentsError):
         self.reason_code = response['Model']['ReasonCode']
         self.cardholder_message = response['Model']['CardHolderMessage']
         super(PaymentError, self).__init__(response, self.reason)
+
+    @property
+    def verbose_reason(self):
+        if self.reason_code not in ERROR_MESSAGES:
+            return None
+        return ERROR_MESSAGES[self.reason_code][0]
+
+    @property
+    def verbose_message(self):
+        if self.reason_code not in ERROR_MESSAGES:
+            return None
+        return ERROR_MESSAGES[self.reason_code][1]
